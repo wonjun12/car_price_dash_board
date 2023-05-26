6f94705523924110645dd35f65bd83da5517feba
wonjun12 <108748094+wonjun12@users.noreply.github.com>
2023-05-26 11:47:10 +0900

    home subheader edit

--- app_module/app_home.py
+++ app_module/app_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # 설명 작성
 def run_app_home():
-    st.subheader('어서와')
+    st.subheader('오랴오랴')
     st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용임!')
 
     st.text('데이터 분석이 가능하고, 고객 정보를 넣으면 구매 차량 가격도 예측 해줌!')
